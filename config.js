d4ec7a8344aec8a17bdc7f987e683ab4051f6b1f
sralee122 <sralee122@gmail.com>
2026-07-03 14:46:55 +0900

    실제 OAuth 클라이언트 ID 반영

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // GCP 콘솔 > API 및 서비스 > 사용자 인증 정보에서 발급받은 OAuth 클라이언트 ID로 교체하세요.
 // 공개 저장소에 노출돼도 무방한 값입니다 (client_secret이 아님).
-export const CLIENT_ID = 'YOUR_CLIENT_ID.apps.googleusercontent.com';
+export const CLIENT_ID = '477548989057-noq5kvg2vrt6skv7lurdhjn8puipblpo.apps.googleusercontent.com';
 
 // 원본 가계부 템플릿 스프레드시트 ID (수정하지 마세요).
 export const TEMPLATE_FILE_ID = '1l0d9NVkUR25zUaHjesSxESCTbAQVxYQ8cy822hVEt8w';
